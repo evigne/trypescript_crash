cf64316deae6241395fb04e6446eccf248e9fb71
evigne <vigneshwaranethirajan@gmail.com>
2019-02-19 19:16:39 +0530

    first commit  typescript

---
+++ types.js
@@ -0,0 +1,22 @@
+// Basic Data Types
+var myString;
+var myNumber;
+var myAge; // booleab,null,etc..
+myString = "Hello World";
+myNumber = 1;
+myAge = "twenty seven";
+// Array
+var strArray;
+var strList = ["Vignesh", "Vicky"];
+var numArray; // boolean[]
+var numList = [1, 2, 3]; // Another method to declare array
+strArray = ["hello", "world"];
+numArray = [1, 2, 3, 4, 5];
+// Tuple Array with defined number of elements
+var strNumTuple;
+strNumTuple = ["Vignesh", 5];
+console.log(numArray);
+//
+var myVoid = undefined;
+var myNull = null;
+var myUndef = undefined;
---
+++ types.ts
@@ -0,0 +1,31 @@
+// Basic Data Types
+
+let myString: string;
+let myNumber: number;
+let myAge: any; // booleab,null,etc..
+myString = "Hello World";
+myNumber = 1;
+myAge = "twenty seven";
+
+// Array
+
+let strArray: string[];
+let strList: Array<string> = ["Vignesh", "Vicky"];
+let numArray: number[]; // boolean[]
+let numList: Array<number> = [1, 2, 3]; // Another method to declare array
+
+strArray = ["hello", "world"];
+numArray = [1, 2, 3, 4, 5];
+
+// Tuple Array with defined number of elements
+
+let strNumTuple: [string, number];
+strNumTuple = ["Vignesh", 5];
+
+console.log(numArray);
+
+//
+
+let myVoid: void = undefined;
+let myNull: null = null;
+let myUndef: undefined = undefined;
